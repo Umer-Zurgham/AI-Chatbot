d5af76a5e2f7d9c5be72cac6a95b793ccd2dbde1
Umer-Zurgham <umerzurgham22226@gmail.com>
2024-07-30 02:45:14 -0600

    knnk

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 
+
 const app = express();
 
 const port = 3000;
 
 // Middlewares
 app.use(express.json());
+
+app.get('/', (req, res)=>{
+   res.send("Hello World");
+})
 
 //connections and listeners
 app.listen(port,()=>{
